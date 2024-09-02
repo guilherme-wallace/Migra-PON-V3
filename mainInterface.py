c5f997805d555ceb286fc03d6c23670ea2f86535
Guilherme Wallace <guilherme.wallace13@hotmail.com>
2024-09-02 15:44:04 -0300

    .

--- mainInterface.py
+++ mainInterface.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from tkinter import *
 from route.dadosConexaoOLTs import *
 from public.currentPort import *
 from public.jsonONUs import *
@@ -23,7 +24,32 @@ olt_IPS = {
 username = user
 password = user_password
 
-def run_script():
+
+def run_scriptAfter():
+    try:
+        # Coleta as entradas do usuário
+        use_OLT_Antiga = olt_antiga_var.get()
+        use_OLT_Nova = olt_nova_var.get()
+        pon_ANTIGA = pon_antiga_var.get()
+        onu_ID = int(onu_id_var.get())
+        ont_LIN_PROF = None  # Configurações podem ser customizadas conforme necessário
+        ont_SRV_PROF = None
+        ont_native_vlan = None
+        ont_vlan_service_port = None
+        ont_gem_PORT = None
+        ont_user_vlan = None
+
+        hostnameOLTAntiga = olt_IPS.get(use_OLT_Antiga)
+        hostnameOLTNova = olt_IPS.get(use_OLT_Nova)
+
+        # Executa as funções principais
+        
+        messagebox.showinfo("Sucesso", "Script executado com sucesso!")
+
+    except Exception as e:
+        messagebox.showerror("Erro", f"Ocorreu um erro: {e}")
+
+def run_scriptLate():
     try:
         # Coleta as entradas do usuário
         use_OLT_Antiga = olt_antiga_var.get()
@@ -57,31 +83,68 @@ def run_script():
 
 # Criação da interface gráfica
 root = tk.Tk()
-root.title("Interface de Autorização de ONUs")
+root.title("Migração de ONUs")
 
+varRows = 0
 # Labels e Caixas de Texto
-tk.Label(root, text="OLT Antiga").grid(row=0, column=0, padx=10, pady=10)
+tk.Label(root, text="OLT Antiga").grid(row= varRows + 1, column=0, padx=20, pady=10)
 olt_antiga_var = tk.StringVar()
-tk.Entry(root, textvariable=olt_antiga_var).grid(row=0, column=1, padx=10, pady=10)
+tk.Entry(root, textvariable=olt_antiga_var).grid(row= varRows + 1, column=1, padx=20, pady=10)
 
-tk.Label(root, text="OLT Nova").grid(row=1, column=0, padx=10, pady=10)
+tk.Label(root, text="OLT Nova").grid(row= varRows + 2, column=0, padx=20, pady=10)
 olt_nova_var = tk.StringVar()
-tk.Entry(root, textvariable=olt_nova_var).grid(row=1, column=1, padx=10, pady=10)
+tk.Entry(root, textvariable=olt_nova_var).grid(row= varRows + 2, column=1, padx=20, pady=10)
 
-tk.Label(root, text="PON Antiga").grid(row=2, column=0, padx=10, pady=10)
+tk.Label(root, text="PON Antiga").grid(row= varRows + 3, column=0, padx=20, pady=10)
 pon_antiga_var = tk.StringVar()
-tk.Entry(root, textvariable=pon_antiga_var).grid(row=2, column=1, padx=10, pady=10)
+tk.Entry(root, textvariable=pon_antiga_var).grid(row= varRows + 3, column=1, padx=20, pady=10)
 
-tk.Label(root, text="ONU ID").grid(row=3, column=0, padx=10, pady=10)
+tk.Label(root, text="ONU ID").grid(row= varRows + 4, column=0, padx=20, pady=10)
 onu_id_var = tk.StringVar()
-tk.Entry(root, textvariable=onu_id_var).grid(row=3, column=1, padx=10, pady=10)
+tk.Entry(root, textvariable=onu_id_var).grid(row= varRows + 4, column=1, padx=20, pady=10)
 
-tk.Label(root, text="VLAN Service Port").grid(row=4, column=0, padx=10, pady=10)
+#ont_LIN_PROF = None
+tk.Label(root, text="Line profile").grid(row= varRows + 5, column=0, padx=20, pady=10)
+LIN_PROF_var = tk.StringVar()
+tk.Entry(root, textvariable=LIN_PROF_var).grid(row= varRows + 5, column=1, padx=20, pady=10)
+
+#ont_SRV_PROF = None
+tk.Label(root, text="Service profile").grid(row= varRows + 6, column=0, padx=20, pady=10)
+SRV_PROF_var = tk.StringVar()
+tk.Entry(root, textvariable=SRV_PROF_var).grid(row= varRows + 6, column=1, padx=20, pady=10)
+
+#ont_native_vlan = None
+tk.Label(root, text="Native VLAN").grid(row= varRows + 7, column=0, padx=20, pady=10)
+native_vlan_var = tk.StringVar()
+tk.Entry(root, textvariable=native_vlan_var).grid(row= varRows + 7, column=1, padx=20, pady=10)
+
+#ont_vlan_service_port = None
+tk.Label(root, text="VLAN Service Port").grid(row= varRows + 8, column=0, padx=20, pady=10)
 vlan_service_port_var = tk.StringVar()
-tk.Entry(root, textvariable=vlan_service_port_var).grid(row=4, column=1, padx=10, pady=10)
+tk.Entry(root, textvariable=vlan_service_port_var).grid(row= varRows + 8, column=1, padx=20, pady=10)
 
-# Botão de execução
-tk.Button(root, text="Executar Script", command=run_script).grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+#ont_gem_PORT = None
+tk.Label(root, text="GEM Port").grid(row= varRows + 9, column=0, padx=20, pady=10)
+gem_PORT_var = tk.StringVar()
+tk.Entry(root, textvariable=gem_PORT_var).grid(row= varRows + 9, column=1, padx=20, pady=10)
+
+#ont_user_vlan = None
+tk.Label(root, text="User VLAN").grid(row= varRows + 10, column=0, padx=20, pady=10)
+user_vlan_var = tk.StringVar()
+tk.Entry(root, textvariable=user_vlan_var).grid(row= varRows + 10, column=1, padx=20, pady=10)
+
+
+oltsName = 'Os nomes das OLTs são: \nOLT-SEA01 \nOLT-SEA03 \nOLT-VTA01 \nOLT-VTA02 \nOLT-VVA01 \nOLT-VVA03 \nOLT-CCA01'
+
+#oltsName_txt = tk.Text(root,width=60,height=10)
+#oltsName_txt.grid(row=0, rowspan=1, column=0, columnspan=1)
+#oltsName_txt.insert(INSERT, oltsName)
+
+# Botão de execução antes
+#tk.Button(root, text="Executar Script Após a migração", command=run_scriptAfter).grid(row=10, column=0, columnspan=2, padx=20, pady=10)
+
+# Botão de execução depois
+tk.Button(root, text="Executar após a migração", command=run_scriptLate).grid(row= varRows + 11, column=0, columnspan=2, padx=20, pady=10)
 
 # Iniciar o loop da interface gráfica
 root.mainloop()
